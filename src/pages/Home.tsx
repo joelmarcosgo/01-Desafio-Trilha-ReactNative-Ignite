2812ace573b16ffca35c394144c9c6a62a9b404c
joelmarcosgo <marcos@gtcommerce.com.br>
2021-08-27 18:27:15 -0300

    Completed-Task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,29 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTask => [...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+
+    const markAsDone = tasks.map(task => task.id === id ? {
+      ...task, done: !task.done
+    } : task)
+
+    setTasks(markAsDone)
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const removeTask = tasks.filter(task => task.id !== id)
+
+    setTasks(removeTask);
   }
 
   return (
